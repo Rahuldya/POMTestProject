49f15c46a7529d15dffe987ca048abecf17d5fa2
eyyvrrl <eyyvrrl@L9AGL57.ericsson.se>
2019-05-03 11:53:06 +0530

    addedLoopClass1

---
+++ OCSW-12/src/InheritanceExm/LoopClass.java
@@ -0,0 +1,15 @@
+package InheritanceExm;
+
+public class LoopClass {
+
+	public static void main(String[] args) {
+		String s[]={"rahul","singh","yadav","gopal","ram"};
+		
+		for(int i=0;i<s.length;i++)
+		{
+			System.out.println(s[i]);
+		}
+
+	}
+
+}
