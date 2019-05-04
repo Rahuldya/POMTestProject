903dbaef8176b4e90b78c8b3cd7631cd3bf24745
eyyvrrl <eyyvrrl@L9AGL57.ericsson.se>
2019-05-04 15:57:28 +0530

    Add to Branch-Y

---
+++ OCSW-12/src/InheritanceExm/TestClassJava.java
@@ -0,0 +1,12 @@
+package InheritanceExm;
+
+public class TestClassJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\\
+		
+		System.out.println("This is Test1 class in Branch-Y now will commit to Main Branch");
+
+	}
+
+}
