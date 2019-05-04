503cfdfa11c9d5d22e51a2132eb0a0003a49fc00
eyyvrrl <eyyvrrl@L9AGL57.ericsson.se>
2019-05-04 15:42:20 +0530

    ArrayList

---
+++ OCSW-12/src/javaExmple/ArrayListFunction.java
@@ -0,0 +1,32 @@
+package javaExmple;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListFunction {
+
+	public static void main(String[] args) {
+    ArrayList<String> l1 =new <String>ArrayList();
+		
+		System.out.println(l1.isEmpty());
+		
+		l1.add("rahul");
+		l1.add("singh ");
+		l1.add("yadav");
+		
+		l1.remove(1);
+	//	l1.clear();
+		
+		int a=l1.indexOf("yadav");
+		
+		System.out.println(a);
+		
+		Iterator it=l1.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+		}
+
+	}
+
+}
